908440cb4d64f3a9c66e20445e1102954033778d
tiagoaabreu <tiagoabreu142002@gmail.com>
2025-04-04 19:40:03 -0300

    a

---
+++ js_tipos_var_func/desafioModulo01Aula02.js
@@ -0,0 +1,34 @@
+/*
+1-let nome = "tiago";
+
+let numero = 1;
+
+let verdadeiro = true;
+
+console.log(typeof nome, typeof numero, typeof verdadeiro);
+*/
+
+/*
+2-
+var nome = "tiago";
+
+var sobrenome = "abreu";
+
+var concatenacao = "o nome é " + nome + " e o sobrenome é " + sobrenome;
+
+var template = `${nome} com o sobrenome ${sobrenome}`;
+
+console.log(concatenacao);
+
+console.log(template);
+*/
+/*
+4-
+var numero = 10;
+
+var str = "Valor";
+
+var texto = `O ${str} do dinheiro é de ${numero}`;
+
+console.log(texto);
+*/
