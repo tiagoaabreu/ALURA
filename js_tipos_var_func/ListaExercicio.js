2cd2884f90dda7b181f209c3b36e0cc9eb397689
tiagoaabreu <tiagoabreu142002@gmail.com>
2025-04-07 13:19:47 -0300

    Update ListaExercicio.js

--- js_tipos_var_func/ListaExercicio.js
+++ js_tipos_var_func/ListaExercicio.js
@@ -15,10 +15,37 @@ console.log(
 
 
 */
-//EXERCICIO NUMERO 2 DA AULA 04 DE JAVASCRIPT
+/* EXERCICIO NUMERO 2 DA AULA 04 DE JAVASCRIPT
 
 let numero = 3;
 
 let resultado = numero % 2 === 0 ? "numero é par" : "numero é ímpar";
 
 console.log(resultado);
+*/
+
+/*
+NUMERO 3 DA AULA 04 DE JAVASCRIPT
+
+let logado = true;
+let admin = false;
+
+let usuario = null;
+
+usuario = logado;
+
+if (usuario === logado) {
+  console.log("Usuario logado");
+} else {
+  console.log("Usuario não logado");
+}
+
+admin = true;
+
+usuario === admin && logado
+  ? console.log("Usuario está logado e é admin")
+  : console.log("Usuario não está logado e não admin");
+
+
+
+*/
