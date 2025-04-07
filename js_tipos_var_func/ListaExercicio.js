43e4aa525682fd7d76e2107dd7e8c66a40aec84b
tiagoaabreu <tiagoabreu142002@gmail.com>
2025-04-07 12:34:13 -0300

    Create lista de exercicios aula 04 de javascript da alura

---
+++ js_tipos_var_func/ListaExercicio.js
@@ -0,0 +1,19 @@
+/*
+EXERCICIO NUMERO 1 DA AULA 04 DE JAVASCRIPT
+
+
+let saldo = 100;
+let deposito = 50;
+let saque = 60;
+
+let operacao = saldo + deposito - saque;
+
+console.log(
+  `o saldo antes de fazer a primeira transação é de: ${saldo}, após isso, foi depositado um valor de: ${deposito}, e foi sacado um valor de: ${saque}, o saldo final é de: ${operacao}`,
+);
+
+
+
+*/
+//EXERCICIO NUMERO 2 DA AULA 04 DE JAVASCRIPT
+
