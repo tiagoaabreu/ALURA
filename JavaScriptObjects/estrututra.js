781a20164b8d457218c1e988ad3368d1b029ef74
tiagoaabreu <tiagoabreu142002@gmail.com>
2025-04-16 09:00:11 -0300

    added JavaScriptObjects/estrututra.js and JavaScriptObjects/objeto.js files

---
+++ JavaScriptObjects/estrututra.js
@@ -0,0 +1,6 @@
+const objPessoa = {
+    nome:"José",
+    idade: 32,
+    cpf : '123.456.789-00',
+    turma: 'javascript'
+}
---
+++ JavaScriptObjects/objeto.js
@@ -0,0 +1,7 @@
+const objPessoa = {
+    nome:"José",
+    idade: 32,
+    cpf : '123.456.789-00',
+    turma: 'javascript'
+}
+
